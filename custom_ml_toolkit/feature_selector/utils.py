4a6335e7f218d34799351b1bc621dd02fa4a4fa7
unknown <chulanutrakul@gmail.com>
2024-09-17 14:31:47 +0700

    add split_feature_category

--- custom_ml_toolkit/feature_selector/utils.py
+++ custom_ml_toolkit/feature_selector/utils.py
@@ -11,12 +11,11 @@ def melt_list_of_list(list_of_list):
             result_list.append(member)
     return result_list
 
-
 def split_cal_and_num(
     col_names: list,
     all_cat_cols: list,
     all_num_cols: list
-) -> Tuple[pd.DataFrame, pd.DataFrame]:
+) -> Tuple[list, list]:
     cat_columns = list()
     num_columns = list()
     for col_name in col_names:
@@ -25,3 +24,20 @@ def split_cal_and_num(
         elif col_name in all_num_cols:
             num_columns.append(col_name)
     return num_columns, cat_columns
+
+def split_feature_category(
+    col_names: list,
+    categories: dict
+) -> dict:
+
+    result = dict()
+    for category in categories:
+        result[category] = list()
+
+    for col_name in col_names:
+        for category in categories:
+            if col_name in categories[category]:
+                result[category].append(col_name)
+                continue
+
+    return result
